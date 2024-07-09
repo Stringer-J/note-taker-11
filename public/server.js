351131e0b67cccfc98c833269c14f9ef1133d71a
Josh Stringer <joshstringer@live.com>
2024-07-09 11:24:19 -0500

    note taker button now actually goes to right place when clicked

--- public/server.js
+++ public/server.js
@@ -4,21 +4,24 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json()); //incoming objects recognized as json
 app.use(express.urlencoded({ extended: true }));
 
 //sets root directory to 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
+//makes the start of the route /api
 const api = express.Router();
 app.use('/api', api);
 
 //sets up first route to index
+//localhost:3000/api
 api.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 //sets up route to notes
+//localhost:3000/api/notes
 api.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'notes.html'));
 });
