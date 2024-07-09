6cae95e34ca1764743e27b351e4ce14e0d5bda40
Josh Stringer <joshstringer@live.com>
2024-07-09 11:07:30 -0500

    added /api to each route, added a todo list for myself

--- public/server.js
+++ public/server.js
@@ -4,16 +4,26 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//sets root directory to 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+const api = express.Router();
+app.use('/api', api);
+
+//sets up first route to index
+api.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/notes', (req, res) => {
+//sets up route to notes
+api.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'notes.html'));
 });
 
+//allows the server to actually run
 app.listen(PORT, () => {
   console.log('Server is running');
 });
