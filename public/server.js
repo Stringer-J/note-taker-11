811a6ab921d9e5e83b0ac2b1adfe84c7ef8d59ea
Josh Stringer <joshstringer@live.com>
2024-07-10 10:23:12 -0500

    set up the correct route for the css file and now it works, very happy about that

--- public/server.js
+++ public/server.js
@@ -27,6 +27,10 @@ app.get('/assets/js/index.js', (req, res) => {
   res.sendFile(path.join(__dirname, 'assets', 'js', 'index.js'));
 });
 
+app.get('/assets/css/styles.css', (req, res) => {
+  res.sendFile(path.join(__dirname, 'assets', 'css', 'styles.css'));
+});
+
 //allows the server to actually run
 app.listen(PORT, () => {
   console.log('Server is running');
