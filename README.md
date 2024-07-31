c412b759bce538f2d91913651c259f9256c40113
Josh Stringer <joshstringer@live.com>
2024-07-31 14:12:13 -0500

    pushing changes from a while back, guessing it's the save note feature and partial readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # note-taker-11
+
+Description: This repo was created to showcase a
+
+Installation: Install all necessary dependencies by running 'npm i' in your terminal.
+
+Usage: To use this repo,
+
+Credits: Josh Stringer
+
+License: Refer to the LICENSE in the repo.
+
+Link To Render Application:
--- public/server.js
+++ public/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const express = require('express'); //lets me use express
+const path = require('path'); //lets me use path
+const fs = require('fs'); //lets me use fs
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const app = express();  //makes 'app' an instance of express
+const PORT = process.env.PORT || 3001; //sets port to 3001 regardless of actual port being used?
 
 app.use(express.json()); //incoming objects recognized as json
 app.use(express.urlencoded({ extended: true }));
@@ -12,37 +12,44 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //sets up first route to index
-//localhost:3000/
+//localhost:3001/
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 //sets up route to notes
-//localhost:3000/notes
+//localhost:3001/notes
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'notes.html'));
 });
 
+//gets the index.js file so we can use it
 app.get('/assets/js/index.js', (req, res) => {
   res.sendFile(path.join(__dirname, 'assets', 'js', 'index.js'));
 });
 
+//gets the styles.css file so we can use it
 app.get('/assets/css/styles.css', (req, res) => {
   res.sendFile(path.join(__dirname, 'assets', 'css', 'styles.css'));
 });
 
+//gets the db.json so we can read and write notes to and from it
 app.get('/api/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../db', 'db.json'));
 });
 
+//creates an array for saving notes to
 let notes = [];
 
+//function that writes notes to db.json
 const saveNotes = () => {
   const filePath = path.resolve(__dirname, '../db/db.json');
-  fs.writeFileSync(filePath, JSON.stringify(notes, null, 2), 'utf8');
+  fs.writeFileSync(filePath, JSON.stringify(notes, null, 2), 'utf8'); //writes file, stringifies 'notes' and makes the output object look nicer for people
 };
 
+//function that generates an id for each note
 const generateId = (array) => {
+
   const maxId = array.reduce((max, item) => (item.id > max ? item.id : max), 0);
   let nextId = maxId + 1;
   return array.map(item => ({
@@ -51,16 +58,22 @@ const generateId = (array) => {
   }));
 };
 
+//post request that actually adds each note to db.json
 app.post('/api/notes', (req, res) => {
-  const { title, text } = req.body;
-  const newNote = {
+
+  const { title, text } = req.body; //makes an object that used the user text for title and text
+  const newNote = { //creates newNote with null id to be changed later and the title and text from before
     id: null, title, text
   };
-  notes.push(newNote);
+
+  notes.push(newNote); //pushes newNote object to the notes array i made earlier
+
   const notesWithId = generateId(notes);
   const updatedNote = notesWithId.find(note => note.title === newNote.title && note.text === newNote.text);
   newNote.id = updatedNote.id;
+
   saveNotes(notesWithId);
+
   console.log('new note added:', newNote);
   res.status(201).json(newNote);
 });
